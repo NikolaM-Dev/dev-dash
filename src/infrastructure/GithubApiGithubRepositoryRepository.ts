235fc326873da92d58b94f0c0cac9ef78842f9d0
Juan David Merchan Torres <juan1014166@gmail.com>
2022-12-29 11:41:14 -0500

    feat(infrastructure): Add searchBy method

--- src/infrastructure/GithubApiGithubRepositoryRepository.ts
+++ src/infrastructure/GithubApiGithubRepositoryRepository.ts
@@ -1,3 +1,22 @@
+	private async searchBy(repositoryId: RepositoryId): Promise<GitHubApiResponses> {
+		const repositoryRequests = this.endpoints
+			.map((endpoint) => endpoint.replace("$name", repositoryId.name))
+			.map((endpoint) => endpoint.replace("$organitzation", repositoryId.organitzation))
+			.map((url) =>
+				fetch(url, { headers: { Authorization: `Bearer ${this.personalAccessToken}` } })
+			);
+
+		return Promise.all(repositoryRequests)
+			.then((responses) => Promise.all(responses.map((response) => response.json())))
+			.then(([repositoryData, pullRequests, ciStatus]) => {
+				return {
+					repositoryData: repositoryData as RepositoryData,
+					pullRequests: pullRequests as PullRequest[],
+					ciStatus: ciStatus as CiStatus,
+				};
+			});
+	}
+
 	private urlToId(url: string): RepositoryId {
 		const splitUrl = url.split("/");
 
